26a1864481fdfca25598c286a13e6a1f5c5bacb5
David Martin <david@martinmacias.com>
2017-02-22 17:50:15 +0100

    add license to files

--- player/player/VAST/PNVASTSchema.h
+++ player/player/VAST/PNVASTSchema.h
@@ -1,11 +1,24 @@
 //
-//  PNVASTSchema.h
-//  VAST
+//  Copyright (c) 2017 PubNative
 //
-//  Created by Muthu on 11/21/13.
-//  Copyright (c) 2013 Nexage. All rights reserved.
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
 //
-//  PNVASTSchema.h contains the vast_2.0.1.xsd schema, used in VAST2Parser for validation.
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
 
 unsigned char pubnative_vast_2_0_1_xsd[] = {
   0x3c, 0x3f, 0x78, 0x6d, 0x6c, 0x20, 0x76, 0x65, 0x72, 0x73, 0x69, 0x6f,
--- player/player/VAST/model/PNVASTMediaFile.h
+++ player/player/VAST/model/PNVASTMediaFile.h
@@ -1,12 +1,24 @@
 //
-//  PNVASTMediaFile.h
-//  VAST
+//  Copyright (c) 2017 PubNative
 //
-//  Created by Jay Tucker on 10/15/13.
-//  Copyright (c) 2013 Nexage. All rights reserved.
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
 //
-//  VASTMediaFile is a data structure that contains parameters for the VAST video itself.
-//  The parameters are available from VASTModel, derived from the VAST XML document using the VAS2Parser.  There may be multiple mediaFiles in each VAST document.
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
 
 #import <Foundation/Foundation.h>
 
